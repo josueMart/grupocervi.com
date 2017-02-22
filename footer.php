f5adef0c2c7536a86b372ce5a07c36d72e411cb7
Josue Martinez <josue.mart@live.com.mx>
2017-02-21 18:56:41 -0600

    Primera modificación del footer y header

--- footer.php
+++ footer.php
@@ -13,13 +13,15 @@
                   <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
                   <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
                 </ul>
+                 <h5 class="white-text">Correo electrónico</h5>
+                 <p class="grey-text text-lighten-4">ventas@grupocervi.com</p>
               </div>
             </div>
           </div>
           <div class="footer-copyright">
             <div class="container">
             © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            <a class="grey-text text-lighten-4 right" href="http://solucionesmart.com">solucionesmart.com</a>
             </div>
           </div>
 </footer>   
